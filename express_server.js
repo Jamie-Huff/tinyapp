500cd7440192b0459303b48ac1994a74368763a5
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-07-06 11:54:59 +0000

    downloaded bodyParser package

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,9 +12,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body) //logs the POST request 
+  res.send("ok")
+})
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars)
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 })
 
   app.get("/urls/:shortURL", (req, res) => {
